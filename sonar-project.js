d8460f727ac81bc8dcbc5f218ed7f69456d069db
rajdotccr-git <chaithanyadotraj@gmail.com>
2025-02-04 20:14:42 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.232.60.53:9980/',
+  serverUrl: 'http://54.254.156.109:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': 'squ_4cf1049269a9a9b6a87265d4b54d75630de17149',
+	    'sonar.login': 'squ_378135fe4c6af60e092be7a47b6efba8cd82963a',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
