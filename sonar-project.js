7b5c352f3007089c4f4b7a5e0cfe6350b2ef1b0d
rajdotccr-git <chaithanyadotraj@gmail.com>
2025-02-08 11:39:40 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.254.156.109:9980/',
+  serverUrl: 'http://54.254.182.40:9980/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
